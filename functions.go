eee2661bf31931c1bde1d14ad6df65fd30ecc8a1
Hamid Aria <hamidaria.70@gmail.com>
2022-09-11 15:15:31 +0430

    Creating purified deployment list

--- functions.go
+++ functions.go
@@ -40,17 +40,28 @@ func MergePodMetricMaps(podDetailList []map[string]string, podMetricsDetailList 
 
 func CheckPodRamUsage(configFile Configuration, podInfo []map[string]string) {
 	alert := ""
+	deploymentList := make([]string, 0)
+	allkeys := make(map[string]bool)
+	deploymentListPurified := []string{}
 	for element := range podInfo {
 		ramValue, _ := strconv.Atoi(podInfo[element]["ram"])
 		if ramValue > configFile.Kubernetes.Threshold.Ram {
 			alert = fmt.Sprintf("Pod %v from deployment %v has hich ram usage. current ram usage is %v",
 				podInfo[element]["name"], podInfo[element]["deployment"], podInfo[element]["ram"])
+			deploymentList = append(deploymentList, podInfo[element]["deployment"])
 			fmt.Println(alert)
 			if configFile.Slack.Notify {
 				SendSlackPayload(configFile, alert)
 			}
 		}
 	}
+	for _, item := range deploymentList {
+		if _, value := allkeys[item]; !value {
+			allkeys[item] = true
+			deploymentListPurified = append(deploymentListPurified, item)
+		}
+	}
+	fmt.Println(deploymentListPurified)
 }
 
 func SendSlackPayload(configFile Configuration, alert string) {
