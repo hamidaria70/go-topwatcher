5575f72787b6897f5ede78c92ed204e2429213bd
Hamid Aria <hamidaria.70@gmail.com>
2023-07-08 11:41:58 +0330

    Some reordering functions

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,6 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"flag"
 	"log"
 	"os"
 	k8s "topwatcher/pkg/kubernetes"
@@ -34,49 +33,52 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+
+		var alerts []string
+		var target []string
+		InfoLogger.Println("Starting topwatcher...")
+
+		clientSet, config := k8s.GetClusterAccess(&configFile)
+
+		if len(configFile.Kubernetes.Namespaces) > 0 {
+			if k8s.Contain(configFile.Kubernetes.Namespaces, clientSet) {
+				podInfo := k8s.GetPodInfo(clientSet, &configFile, config)
+				if configFile.Logging.Debug {
+					DebugLogger.Printf("Pods information list is: %v", podInfo)
+				}
+				if configFile.Kubernetes.Threshold.Ram > 0 {
+					alerts, target = CheckPodRamUsage(&configFile, podInfo)
+				} else {
+					ErrorLogger.Println("Ram value is not defined in configuration file")
+					os.Exit(1)
+				}
+			} else {
+				WarningLogger.Printf("'%v' namespace is not in the cluster!!", configFile.Kubernetes.Namespaces)
+			}
+		} else {
+			ErrorLogger.Println("Namespace is not defined")
+			os.Exit(1)
+		}
+
+		if len(target) > 0 && configFile.Kubernetes.PodRestart {
+			k8s.RestartDeployment(clientSet, target)
+		}
+
+		if configFile.Slack.Notify && len(configFile.Slack.Channel) > 0 {
+			SendSlackPayload(&configFile, alerts)
+		} else {
+			for _, alert := range alerts {
+				InfoLogger.Println(alert)
+			}
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
-	var alerts []string
-	var target []string
-
-	clientSet, config := k8s.GetClusterAccess(&configFile)
-
-	if len(configFile.Kubernetes.Namespaces) > 0 {
-		if k8s.Contain(configFile.Kubernetes.Namespaces, clientSet) {
-			podInfo := k8s.GetPodInfo(clientSet, &configFile, config)
-			if configFile.Logging.Debug {
-				DebugLogger.Printf("Pods information list is: %v", podInfo)
-			}
-			if configFile.Kubernetes.Threshold.Ram > 0 {
-				alerts, target = CheckPodRamUsage(&configFile, podInfo)
-			} else {
-				ErrorLogger.Println("Ram value is not defined in configuration file")
-				os.Exit(1)
-			}
-		} else {
-			WarningLogger.Printf("'%v' namespace is not in the cluster!!", configFile.Kubernetes.Namespaces)
-		}
-	} else {
-		ErrorLogger.Println("Namespace is not defined")
-		os.Exit(1)
-	}
-
-	if len(target) > 0 && configFile.Kubernetes.PodRestart {
-		k8s.RestartDeployment(clientSet, target)
-	}
-
-	if configFile.Slack.Notify && len(configFile.Slack.Channel) > 0 {
-		SendSlackPayload(&configFile, alerts)
-	} else {
-		for _, alert := range alerts {
-			InfoLogger.Println(alert)
-		}
-	}
 	if err != nil {
 		os.Exit(1)
 	}
@@ -85,7 +87,7 @@ func Execute() {
 func init() {
 	var flags int
 
-	flag.Parse()
+	//flag.Parse()
 
 	configFile = reader.ReadFile()
 	if configFile.Logging.Debug {
@@ -97,8 +99,6 @@ func init() {
 	InfoLogger = log.New(os.Stdout, "INFO ", flags)
 	WarningLogger = log.New(os.Stdout, "WARNING ", flags)
 	ErrorLogger = log.New(os.Stdout, "ERROR ", flags)
-
-	InfoLogger.Println("Starting topwatcher...")
 
 	if configFile.Logging.Debug {
 		DebugLogger.Println("Reading Configuration file...")
